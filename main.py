4f553fe4b82945cae4d475e389dc381921db0869
Antonina Novak <anovak@gmail.com>
2022-12-14 15:29:10 +0200

    argparse's tutorial

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import sys
+import argparse
 
-def task1(filename, country, year):
+def task1():
+    parser = argparse.ArgumentParser(description="Medals parser.")
+    parser.add_argument("--filename", "-f", required=True)
+    parser.add_argument("--medals", action="store_true", required=False)
+    parser.add_argument("--country", required=False)
+    parser.add_argument("--year", required=False)
+    parser.add_argument("--output", "-o", required=False)
     head = None
     is_first_line = True
-    with open(filename, "r") as file:
-        for line in file.readlines():
-            data = line.strip().split("\t")
-            if is_first_line:
-                head = data
-                is_first_line = False
-                continue
-            if country == data[head.index("NOC")] and year == data[head.index("Year")]:
-                pass
+    args = parser.parse_args()
+    if args.medals:
+        with open(args.filename, "r") as file:
+            for line in file.readlines():
+                data = line.strip().split("\t")
+                if is_first_line:
+                    head = data
+                    is_first_line = False
+                    continue
+                if args.country == data[head.index("NOC")] and args.year == data[head.index("Year")]:
+                    print(line.strip())
 
 
 def task2():
-    pass
+    parser = argparse.ArgumentParser(description="Total parser.")
+    parser.add_argument("--filename", "-f", required=True)
+    parser.add_argument("--total", action="store_true", required=False)
+    parser.add_argument("--output", "-o", required=False)  # please include an output into all tasks
+
+def task3():
+    parser = argparse.ArgumentParser(description="Overall parser.")
+
+def task4():
+    parser = argparse.ArgumentParser(description="Interactive parser.")
 
 def main():
-    args = sys.argv
-    if args[1] == "-medals":
-        filename = args[args.index("-filename") + 1]
-        country = args[args.index("-country") + 1]
-        year = args[args.index("-year") + 1]
-        print(filename, country, year)
-        task1(filename, country, year)
+    task1()
 
 
 if __name__ == "__main__":
