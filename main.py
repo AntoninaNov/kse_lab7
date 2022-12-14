e071527e7728b663912a7a7c92ed1481b157d8e3
Antonina Novak <anovak@gmail.com>
2022-12-14 16:29:09 +0200

    argparse's tutorial

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import sys
 import argparse
 
+parser = argparse.ArgumentParser(description="File parser.")
+parser.add_argument("--filename", "-f", required=True)
+parser.add_argument("--medals", action="store_true", required=False)
+parser.add_argument("--country", required=False)
+parser.add_argument("--year", required=False)
+parser.add_argument("--output", "-o", required=False) # please include an output into all tasks
+parser.add_argument("--total", action="store_true", required=False)
+
+
 def task1():
-    parser = argparse.ArgumentParser(description="Medals parser.")
-    parser.add_argument("--filename", "-f", required=True)
-    parser.add_argument("--medals", action="store_true", required=False)
-    parser.add_argument("--country", required=False)
-    parser.add_argument("--year", required=False)
-    parser.add_argument("--output", "-o", required=False)
     head = None
     is_first_line = True
     args = parser.parse_args()
     if args.medals:
+        output_file = None
+        idx = 0
+        if args.output is not None:
+            output_file = open(args.output, "w")
         with open(args.filename, "r") as file:
             for line in file.readlines():
                 data = line.strip().split("\t")
@@ -21,19 +28,25 @@ def task1():
                     continue
                 if args.country == data[head.index("NOC")] and args.year == data[head.index("Year")]:
                     print(line.strip())
+                    if output_file is not None:
+                        idx += 1
+                        output_file.write(str(idx) + ",".join(data) + "\n")
+        if output_file is not None:
+            output_file.close()
+    # файл для виведення результатів (output_file)
 
 
 def task2():
-    parser = argparse.ArgumentParser(description="Total parser.")
-    parser.add_argument("--filename", "-f", required=True)
-    parser.add_argument("--total", action="store_true", required=False)
-    parser.add_argument("--output", "-o", required=False)  # please include an output into all tasks
+    pass
+
 
 def task3():
-    parser = argparse.ArgumentParser(description="Overall parser.")
+    pass
+
 
 def task4():
-    parser = argparse.ArgumentParser(description="Interactive parser.")
+    pass
+
 
 def main():
     task1()
